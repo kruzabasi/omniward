0eb9f49103e2c30aa8d1f2e344fa197afb5ec74a
Kruzabasi <kruzabasi@gmail.com>
2023-06-18 18:09:11 +0100

    app control functions

---
+++ env/dev/user.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require
+   [clojure.java.io :as io]
+   [integrant.core :as ig]
+   [integrant.repl :as ig-repl]))
+
+(ig-repl/set-prep!
+ (fn []
+   (let [config (-> "config.edn"
+                    io/resource
+                    slurp
+                    ig/read-string)]
+     (ig/load-namespaces config)
+     config)))
+
+(defn start-server
+  []
+  (ig-repl/go))
+
+(defn stop-server
+  []
+  (ig-repl/halt))
+
+(defn restart-server
+  []
+  (stop-server)
+  (start-server))
