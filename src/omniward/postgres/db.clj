f1cac807a70a658d3a74f3ee6cc6e29e0d5973fa
Kruzabasi <kruzabasi@gmail.com>
2023-06-20 09:20:19 +0100

    create patients table

--- src/omniward/postgres/db.clj
+++ src/omniward/postgres/db.clj
@@ -1,6 +1,7 @@
 (ns omniward.postgres.db
   (:require [integrant.core :as ig]
-            [integrant.repl.state :refer [system]]))
+            [integrant.repl.state :refer [system]]
+            [clojure.java.jdbc :as j]))
 
 (defmethod ig/init-key ::pg-db
   [_ config]
@@ -8,3 +9,18 @@
 
 (defn get-db []
   (:omniward.postgres.db/pg-db system))
+
+(def patients-sql
+  (j/create-table-ddl
+   :patient 
+   [[:patient_id :serial "PRIMARY KEY"]
+    [:name "VARCHAR(255)"]
+    [:gender "VARCHAR(10)"]
+    [:dob "DATE"]
+    [:address "VARCHAR(255)"]
+    [:phone "VARCHAR(20)"]
+    [:CONSTRAINT :unique_patient "UNIQUE(name, dob)"]]))
+
+(defn create-patients-table []
+  (let [db-spec (get-db)]
+    (j/execute! db-spec patients-sql)))
